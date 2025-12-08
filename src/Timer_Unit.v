514b8c498ef2225e42be0035d8975fcb113f1443
Qiyu Zhang <2534505853@qq.com>
2025-12-08 15:48:08 +0800

    写完Seg7_Driver，顺便调了下Timer_Unit

--- src/Timer_Unit.v
+++ src/Timer_Unit.v
@@ -5,8 +5,8 @@ module Timer_Unit #(
     input wire rst_n,
 
     // --- 控制接口 ---
-    input wire w_start_timer,          // 启动/重置倒计时 (脉冲有效)
-    input wire w_en,             // 倒计时使能 (高电平保持计数，低电平暂停)
+    input wire i_start_timer,          // 启动/重置倒计时 (脉冲有效)
+    input wire i_en,             // 倒计时使能 (高电平保持计数，低电平暂停)
     input wire [3:0] sw, // 倒计时初始值 (例如 10秒)
     
     // --- 状态输出 ---
@@ -18,9 +18,9 @@ module Timer_Unit #(
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             cnt<=0;
-        end else if (w_start_timer) begin 
+        end else if (i_start_timer) begin 
             cnt<=0;
-        end else if (w_en && w_time_val>0) begin
+        end else if (i_en && w_time_val>0) begin
             if (cnt==CLK_FREQ-1) cnt<=0;
             else cnt<=cnt+1;
         end else begin
@@ -32,10 +32,10 @@ module Timer_Unit #(
         if (!rst_n) begin
             w_timeout<=0;
             w_time_val<=4'd10;
-        end else if (w_start_timer) begin
+        end else if (i_start_timer) begin
             w_timeout<=0;
             w_time_val<=sw;
-        end else if (w_en) begin
+        end else if (i_en) begin
             if (cnt==CLK_FREQ-1) begin
                 if (w_time_val>=1) begin
                     if (w_time_val==4'd1) w_timeout<=1;
@@ -50,7 +50,7 @@ module Timer_Unit #(
                 w_time_val<=w_time_val;
             end
         end else begin
-            w_timeout<=w_timeout; 
+            w_timeout<=0; 
             w_time_val<=w_time_val;
         end
     end
